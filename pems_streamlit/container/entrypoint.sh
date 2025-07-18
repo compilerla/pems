9b3417ab10c0597ad36274ceb526cafd15d19266
Kegan Maher <kegan@compiler.la>
2025-07-18 13:21:31 -0700

    refactor(pems_streamlit): move Dockerfile and assets
    update the build into a multi-stage:
    
      - first build the pems_streamlit wheel
      - then install it with other necessary tooling
    
    similar to how the pems_web Docker build works

--- pems_streamlit/container/entrypoint.sh
+++ pems_streamlit/container/entrypoint.sh
@@ -7,7 +7,7 @@ BASE_URL_PATH=${STREAMLIT_BASE_URL:-""}
 echo "Starting Streamlit with base URL path: ${BASE_URL_PATH}"
 
 # Execute the Streamlit command, passing the base URL path
-exec streamlit run streamlit_app/main.py \
+exec streamlit run run.py \
   --server.port=8501 \
   --server.address=0.0.0.0 \
   --server.baseUrlPath="${BASE_URL_PATH}"
