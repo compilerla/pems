5d725f20aa1872ff700256126241e73642e554ca
Kegan Maher <kegan@compiler.la>
2025-07-26 00:22:20 +0000

    refactor(pems_data): more generic services subpackage
    the concept of a Service here is for the "what" of accessing
    specific data for business logic
    
    refactor the StationsBucket to a StationsService
    StationsService needs an IDataSource to read from (the "how")

--- pems_data/src/pems_data/services/stations.py
+++ pems_data/src/pems_data/services/stations.py
@@ -1,13 +1,16 @@
 import pandas as pd
 
-from pems_data.s3 import S3Bucket
+from pems_data.sources import IDataSource
 
 
-class StationsBucket(S3Bucket):
-    """Station-specific bucket data."""
+class StationsService:
+    """Manages fetching of station-related data."""
 
     imputation_detector_agg_5min = "imputation/detector_imputed_agg_five_minutes"
     metadata_file = "geo/current_stations.parquet"
+
+    def __init__(self, data_source: IDataSource):
+        self.data_source = data_source
 
     def get_district_metadata(self, district_number: str) -> pd.DataFrame:
         """Loads metadata for all stations in the selected District from S3."""
@@ -30,7 +33,7 @@ class StationsBucket(S3Bucket):
         ]
         filters = [("DISTRICT", "=", district_number)]
 
-        return self.read_parquet(self.metadata_file, columns=columns, filters=filters)
+        return self.data_source.read(self.metadata_file, columns=columns, filters=filters)
 
     def get_imputed_agg_5min(self, station_id: str) -> pd.DataFrame:
         """Loads imputed aggregate 5 minute data for a specific station."""
@@ -45,4 +48,4 @@ class StationsBucket(S3Bucket):
         ]
         filters = [("STATION_ID", "=", station_id)]
 
-        return self.read_parquet(self.imputation_detector_agg_5min, columns=columns, filters=filters)
+        return self.data_source.read(self.imputation_detector_agg_5min, columns=columns, filters=filters)
