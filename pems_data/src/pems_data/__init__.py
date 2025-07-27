eb6404b2bc6784eba9d0d7801a239230615de1ba
Kegan Maher <kegan@compiler.la>
2025-07-27 02:10:36 +0000

    docs(pems_data): reference for ServiceFactory

--- pems_data/src/pems_data/__init__.py
+++ pems_data/src/pems_data/__init__.py
@@ -11,11 +11,40 @@ class ServiceFactory:
     Shared dependencies are created once during initialization.
     """
 
+    @property
+    def cache(self) -> Cache:
+        """
+        Returns:
+            value (pems_data.cache.Cache): The shared Cache instance managed by this factory.
+        """
+        return self._cache
+
+    @property
+    def s3_source(self) -> S3DataSource:
+        """
+        Returns:
+            value (pems_data.sources.s3.S3DataSource): The shared S3DataSource instance managed by this factory.
+        """
+        return self._s3_source
+
+    @property
+    def caching_s3_source(self) -> CachingDataSource:
+        """
+        Returns:
+            value (pems_data.sources.cache.CachingDataSource): The shared CachingDataSource instance managed by this factory.
+        """
+        return self._caching_s3_source
+
     def __init__(self):
-        self.cache = Cache()
-        self.s3_source = S3DataSource()
-        self.caching_s3_source = CachingDataSource(data_source=self.s3_source, cache=self.cache)
+        """Initializes a new ServiceFactory and shared dependencies."""
+        self._cache = Cache()
+        self._s3_source = S3DataSource()
+        self._caching_s3_source = CachingDataSource(data_source=self._s3_source, cache=self._cache)
 
     def stations_service(self) -> StationsService:
-        """Creates a fully-configured `StationsService`."""
-        return StationsService(data_source=self.caching_s3_source)
+        """Creates a fully-configured StationsService.
+
+        Returns:
+            value (pems_data.services.stations.StationsService): A StationsService instance configured by the factory.
+        """
+        return StationsService(data_source=self._caching_s3_source)
