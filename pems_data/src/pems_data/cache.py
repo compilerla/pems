da3252789c1b77ab7f5c7549c7e8c8373eedf28d
Kegan Maher <kegan@compiler.la>
2025-07-27 02:11:23 +0000

    docs(pems_data): reference for caching layer

--- pems_data/src/pems_data/cache.py
+++ pems_data/src/pems_data/cache.py
@@ -1,3 +1,5 @@
+"""The primary caching interface for `pems_data`."""
+
 import logging
 import os
 from typing import Any, Callable
@@ -10,14 +12,17 @@ from pems_data.serialization import arrow_bytes_to_df, df_to_arrow_bytes
 logger = logging.getLogger(__name__)
 
 
-def redis_connection(host: str = None, port: int = None, **kwargs) -> redis.Redis | None:
+def redis_connection(host: str = None, port: int = None, **kwargs: dict[str, Any]) -> redis.Redis | None:
     """Try to create a new connection to a redis backend. Return None if the connection fails.
 
     Uses the `REDIS_HOSTNAME` and `REDIS_PORT` environment variables as fallback.
 
     Args:
-        host (str): The redis hostname
-        port (int): The port to connect on
+        host (str): (Optional) The redis hostname
+        port (int): (Optional) The port to connect on
+
+    Returns:
+        value (redis.Redis): A Redis instance connected to `host:port`, or None if the connection failed.
     """
 
     host = host or os.environ.get("REDIS_HOSTNAME", "redis")
@@ -36,43 +41,57 @@ def redis_connection(host: str = None, port: int = None, **kwargs) -> redis.Redi
 
 
 class Cache:
-    """Basic caching interface for `pems_data`."""
+    """Basic wrapper for a cache backend."""
 
     @classmethod
-    def build_key(cls, *args) -> str:
-        """Build a cache key from the given parts."""
+    def build_key(cls, *args: Any) -> str:
+        """Build a standard cache key from the given parts.
+
+        Args:
+            *args (tuple[Any]): The individual parts that make up the key
+
+        Returns:
+            value (str): A standard representation of the parts for use in a cache key.
+        """
         return ":".join([str(a).lower() for a in args])
 
     def __init__(self, host: str = None, port: int = None):
         """Create a new instance of the Cache interface.
 
         Args:
-            host (str): (Optional) The hostname of the cache backend.
-            port (int): (Optional) The port to connect on the cache backend.
+            host (str): (Optional) The hostname of the cache backend
+            port (int): (Optional) The port to connect on the cache backend
         """
 
         self.host = host
         self.port = port
         self.c = None
 
-    def _connect(self):
+    def _connect(self) -> None:
         """Establish a connection to the cache backend if necessary."""
         if not isinstance(self.c, redis.Redis):
             self.c = redis_connection(self.host, self.port)
 
     def is_available(self) -> bool:
-        """Return a bool indicating if the cache backend is available or not."""
+        """Return a bool indicating if the cache backend is available or not.
+
+        Returns:
+            value (bool): True if the connection and backend is available; False otherwise
+        """
         self._connect()
         available = self.c and self.c.ping() is True
         logger.debug(f"cache is available: {available}")
         return available
 
-    def get(self, key: str, mutate_func: Callable[[Any], Any] = None) -> Any:
+    def get(self, key: str, mutate_func: Callable[[Any], Any] = None) -> Any | None:
         """Get a raw value from the cache, or None if the key doesn't exist.
 
         Args:
-            key (str): The item's cache key.
-            mutate_func (callable): If provided, call this on the cached value and return its result.
+            key (str): The item's cache key
+            mutate_func (callable): (Optional) If provided, call this on the cached value and return its result
+
+        Returns:
+            value (Any | None): The value from the cache, optionally mutated by mutate_func, or None.
         """
         if self.is_available():
             logger.debug(f"read from cache: {key}")
@@ -85,17 +104,24 @@ class Cache:
         return None
 
     def get_df(self, key: str) -> pd.DataFrame:
-        """Get a `pandas.DataFrame` from the cache, or None if the key doesn't exist."""
+        """Get a DataFrame from the cache, or an empty DataFrame if the key wasn't found.
+
+        Args:
+            key (str): The item's cache key
+
+        Returns:
+            value (pandas.DataFrame): The DataFrame materialized from the cache, or an empty DataFrame if the key wasn't found.
+        """
         return self.get(key, mutate_func=arrow_bytes_to_df)
 
     def set(self, key: str, value: Any, ttl: int = None, mutate_func: Callable[[Any], Any] = None) -> None:
-        """Set a value in the cache.
+        """Set a value in the cache, with an optional TTL (seconds until expiration).
 
         Args:
-            key (str): The item's cache key.
-            value (Any): The item's value to store in the cache.
-            ttl (int): Seconds until expiration.
-            mutate_func (callable): If provided, call this on the value and insert the result in the cache.
+            key (str): The item's cache key
+            value (Any): The item's value to store in the cache
+            ttl (int): (Optional) Seconds until expiration
+            mutate_func (callable): (Optional) If provided, call this on the value and insert the result in the cache
         """
         if self.is_available():
             if mutate_func:
@@ -107,5 +133,11 @@ class Cache:
             logger.warning(f"cache unavailable to set: {key}")
 
     def set_df(self, key: str, value: pd.DataFrame, ttl: int = None) -> None:
-        """Set a `pandas.DataFrame` in the cache, with an optional TTL (seconds until expiration)."""
-        self.set(key, value, mutate_func=df_to_arrow_bytes, ttl=ttl)
+        """Set a DataFrame in the cache, with an optional TTL (seconds until expiration).
+
+        Args:
+            key (str): The item's cache key
+            value (Any): The DataFrame to store in the cache
+            ttl (int): (Optional) Seconds until expiration
+        """
+        self.set(key, value, ttl=ttl, mutate_func=df_to_arrow_bytes)
--- pems_data/src/pems_data/serialization.py
+++ pems_data/src/pems_data/serialization.py
@@ -1,10 +1,19 @@
+"""Helpers for serializing specific data types e.g. for storing/retrieving from a cache."""
+
 import pandas as pd
 import pyarrow as pa
 import pyarrow.ipc as ipc
 
 
 def arrow_bytes_to_df(arrow_buffer: bytes) -> pd.DataFrame:
-    """Deserializes Arrow IPC format `bytes` back to a `pandas.DataFrame`."""
+    """Deserializes Arrow IPC format bytes back to a DataFrame.
+
+    Args:
+        arrow_buffer (bytes): A buffer of Arrow IPC bytes representing a DataFrame
+
+    Returns:
+        value (pandas.DataFrame): The DataFrame deserialized from the buffer, or an empty DataFrame if the buffer was empty.
+    """
     if not arrow_buffer:
         return pd.DataFrame()
     # deserialize the Arrow IPC stream
@@ -16,7 +25,14 @@ def arrow_bytes_to_df(arrow_buffer: bytes) -> pd.DataFrame:
 
 
 def df_to_arrow_bytes(df: pd.DataFrame) -> bytes:
-    """Serializes a `pandas.DataFrame` to Arrow IPC format `bytes`."""
+    """Serializes a DataFrame to Arrow IPC format bytes.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame to serialize
+
+    Returns:
+        value (bytes): The Arrow IPC format bytes representation of the DataFrame.
+    """
     if df.empty:
         return b""
     # convert DataFrame to an Arrow Table
